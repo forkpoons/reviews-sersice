491e7611014df63ffa4b086cee6ac0eddf7e6a6f
fork <forkpoons@gmail.com>
2025-02-18 16:29:46 +0700

    1

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module reviews-sersice
 
-go 1.22
+go 1.23.4
+
+replace github.com/ItemCloudShopping/library/pg => C:\Users\plotnikovay\GolandProjects\library\pg
+replace github.com/ItemCloudShopping/library/probes => C:\Users\plotnikovay\GolandProjects\library\probes
+replace github.com/ItemCloudShopping/library/tracing => C:\Users\plotnikovay\GolandProjects\library\tracing
+replace github.com/ItemCloudShopping/library/yamlreader => C:\Users\plotnikovay\GolandProjects\library\yamlreader
+replace github.com/ItemCloudShopping/library/zerohook => C:\Users\plotnikovay\GolandProjects\library\zerohook
---
+++ internal/config/config.go
@@ -0,0 +1,25 @@
+package config
+
+type ApiConfig struct {
+	//Port *yamlenv.Env[int] `yaml:"port"`
+	test string
+}
+
+type AppConfig struct {
+	//Probe  probes.ProbeConfig `yaml:"probe"`
+	Secret string `yaml:"secret"`
+}
+
+type Config struct {
+	//Log      zerohook.LoggerConfig `yaml:"log"`
+	App AppConfig `yaml:"app"`
+	Api ApiConfig `yaml:"api"`
+	//Postgres pg.PostgresConfig     `yaml:"postgres"`
+	//Kafka    struct {
+	//	Broker     *yamlenv.Env[string] `yaml:"broker"`
+	//	Topic      *yamlenv.Env[string] `yaml:"topic"`
+	//	GroupID    *yamlenv.Env[string] `yaml:"group_id"`
+	//	MaxRetries *yamlenv.Env[int]    `yaml:"max_retries"`
+	//} `yaml:"kafka"`
+	//Jaeger tracing.JaegerConfig `yaml:"jaeger"`
+}
